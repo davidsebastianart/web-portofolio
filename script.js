5cef6f35aa9d1985fcd213fc10c1e76ab0b15008
David Sebastian <114891645+davidsebastianart@users.noreply.github.com>
2025-05-20 23:29:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth Scrolling for the navigation links
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
